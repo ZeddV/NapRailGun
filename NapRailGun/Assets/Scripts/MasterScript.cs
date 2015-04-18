9d18554dbf4fc79c8ea058dea40a129cd1544b1e
Zedd <t.abdel-aziz@gmx.at>
2015-04-19 00:39:52 +0200

    StatusControl and MasterScript

---
+++ NapRailGun/Assets/Scripts/MasterScript.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MasterScript : MonoBehaviour {
+
+	public int playerQuantity = 4;
+	public bool pinkMode = false;
+	public GameObject prefabPlayer;
+	public GameObject prefabStatus;
+	public Transform canvasStatus;
+	public Text txtMid;
+
+	bool PlayerAuswahl = true;
+	bool finish = false; 
+	bool begin = true;
+	bool timebegin = false;
+	bool pause = false;
+	float tempTime; 
+	int secondsOnBegin = 1;
+
+	GameObject grayLayer;
+	GameObject pinkLayer;
+	ArrayList players = new ArrayList();
+	ArrayList playerStatus = new ArrayList();
+	Vector3[] playerStatusPosition = new Vector3[4];
+
+	// Use this for initialization
+	void Start () {
+		playerStatusPosition[0] = new Vector3(-681.5f, -129.6f, 0);
+		playerStatusPosition[1] = new Vector3(-373.96f, -129.6f, 0);
+		playerStatusPosition[2] = new Vector3(-681.5f, -254f, 0);
+		playerStatusPosition[3] = new Vector3(-373.96f, -254f, 0);
+		pinkLayer = GameObject.Find ("PinkLayer");
+		grayLayer = GameObject.Find ("GrayLayer");
+		if(!pinkMode){
+			pinkLayer.SetActive(false);
+		}
+		GameObject panel;
+		for(int i = 0; i < playerQuantity; i++){
+			panel = Instantiate (prefabStatus, playerStatusPosition[i], prefabStatus.transform.rotation ) as GameObject;
+			panel.transform.parent = canvasStatus;
+			panel.GetComponent<RectTransform>().anchoredPosition = playerStatusPosition[i];
+			panel.GetComponent<RectTransform>().localScale = new Vector3(0.4f, 0.4f, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(begin){
+			Time.timeScale = 0;
+			if(!timebegin){
+				tempTime = Time.unscaledTime;
+				timebegin = true;
+			} else{
+				if(Time.unscaledTime > tempTime+2){
+					secondsOnBegin += 1;
+					tempTime = Time.unscaledTime;
+					txtMid.text = secondsOnBegin+"";
+					if(secondsOnBegin == 4){
+						txtMid.text = "Fight!";
+						Time.timeScale = 1;
+						grayLayer.SetActive(false);
+					} else if(secondsOnBegin == 5){
+						begin = false;
+						Time.timeScale = 1;
+						txtMid.transform.gameObject.SetActive(false);
+					}
+				}
+			}
+		}
+		if(finish){
+
+		}
+		if(!begin){
+			if(Input.GetKeyDown(KeyCode.P)){
+				if(pause){
+					pause = false;
+					Time.timeScale = 1;
+					grayLayer.SetActive(false);
+					txtMid.transform.gameObject.SetActive(false);
+				} else {
+					pause = true;
+					Time.timeScale = 0;
+					grayLayer.SetActive(true);
+					txtMid.transform.gameObject.SetActive(true);
+					txtMid.text = "Pause";
+				}
+			}
+		}
+
+
+
+	}
+
+
+}
---
+++ NapRailGun/Assets/Scripts/StatusControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatusControl : MonoBehaviour {
+
+	public Image healthFill; 
+	public Image energyFill;
+	public Text txtName;
+	
+	float energy = 100f;
+	float health = 100f;
+
+	void Awake(){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void subEnergy(float energy){
+		this.energy -= energy;
+		if(this.energy < 0){
+			this.energy = 0;
+		}
+		energyFill.fillAmount = (1f/100f)*this.energy;
+	}
+
+	void addEnergy(float energy){
+		this.energy += energy;
+		if(this.energy > 100){
+			this.energy = 100;
+		}
+		energyFill.fillAmount = (1f/100f)*this.energy;
+	}
+
+	void subHealth(float health){
+		this.health -= health;
+		if(this.health < 0){
+			this.health = 0;
+		}
+		healthFill.fillAmount = (1f/100f)*this.health;
+	}
+	
+	void addHealth(float health){
+		this.health += health;
+		if(this.health > 100){
+			this.health = 100;
+		}
+		healthFill.fillAmount = (1f/100f)*this.health;
+	}
+
+	float getHealth() {
+		return health;
+	}
+
+	float getEnergy(){
+		return energy;
+	}
+
+	void setName(string name){
+		txtName.text = name;
+	}
+}
